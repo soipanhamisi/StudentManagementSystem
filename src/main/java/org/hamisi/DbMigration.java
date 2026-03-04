6d5af143d1f9fd35eece184118f1136a19c10e2c
soipanhamisi <soipanhani@gmail.com>
2026-03-04 16:20:45 +0300

    hiding db passwords...

--- src/main/java/org/hamisi/DbMigration.java
+++ src/main/java/org/hamisi/DbMigration.java
@@ -1,13 +1,17 @@
 package org.hamisi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.flywaydb.core.Flyway;
 
 public class DbMigration {
     public static void main(String[] args) {
 //      database-setup program
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        String pass = dotenv.get("DB_PASS");
+        String user = dotenv.get("DB_USER");
         String url = "jdbc:mysql://localhost:3306/collegeDB";
-        String user = "root";
-        String pass = "tiktok123";
+
+
         Flyway flyway = Flyway.configure()
                 .dataSource(url, user, pass)
                 .baselineOnMigrate(true)
