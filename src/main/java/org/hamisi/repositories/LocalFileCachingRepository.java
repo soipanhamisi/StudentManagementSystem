c0dfc1156425bbf20fc828a5aba93d397c5dcbdf
soipanhamisi <soipanhani@gmail.com>
2026-03-30 12:29:56 +0300

    feat: add file caching abstraction methods to LocalFileCachingRepository

--- src/main/java/org/hamisi/repositories/LocalFileCachingRepository.java
+++ src/main/java/org/hamisi/repositories/LocalFileCachingRepository.java
@@ -3,7 +3,10 @@ package org.hamisi.repositories;
 import org.hamisi.models.Student;
 
 public interface LocalFileCachingRepository {
+    public void initFile();
+    public void loadCache();
     public void putStudent(Student student);
-    public Student getStudent();
+    public Student getStudent(int id);
     public void clearStudent();
+    public void commitToCache();
 }
